cee45467ce486f6211a32fad3a9f017facab1e74
Donald Propst <donald.propst@contrastsecurity.com>
2020-06-08 13:04:22 -0400

    RUBY-888 add xxe routes and specs

---
+++ spec/app/controllers/vulneruby_engine/xxe_controller_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+RSpec.describe('XXE Controller', type: :request) do
+  describe 'GET /xxe' do
+    it 'renders the xxe input page' do
+      get '/vulneruby_engine/xxe'
+      expect(response).to(render_template(:index))
+    end
+  end
+
+  describe 'POST /xxe' do
+    it 'renders the xxe result page' do
+      post '/vulneruby_engine/xxe'
+      expect(response).to(render_template(:run))
+      expect(response.body).to(include('xml'))
+    end
+  end
+end
