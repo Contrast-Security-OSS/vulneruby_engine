aad2737f550a66fc3d1048c80eb94b70a9c8eeb8
Donald Propst <donald.propst@contrastsecurity.com>
2020-07-09 14:55:38 -0400

    RUBY-905-RUBY-909 add reflection injection and path traversal samples

---
+++ spec/app/controllers/vulneruby_engine/path_traversal_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+RSpec.describe('Path Traversal Controller', type: :request) do
+  describe 'GET /path_traversal' do
+    it 'renders the path traversal input page' do
+      get '/vulneruby_engine/path_traversal'
+      expect(response).to(render_template(:index))
+    end
+  end
+
+  describe 'POST /cmdi' do
+    it 'renders any requested file contents' do
+      post '/vulneruby_engine/path_traversal', params: { file_path: '/etc/passwd' }
+      expect(response).to(render_template(:run))
+      expect(response.body).to(include('etc'))
+    end
+  end
+end
