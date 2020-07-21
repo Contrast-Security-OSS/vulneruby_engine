1b7c2bb2d29da173ab0cc21ee4429e3b805e2e53
Donald Propst <donald.propst@contrastsecurity.com>
2020-07-21 11:01:25 -0400

    RUBY-910 add regex dos controller

---
+++ spec/app/controllers/vulneruby_engine/regex_dos_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.describe('Regex DoS Controller', type: :request) do
+  describe 'GET /regex_dos' do
+    it 'renders the regex dos input page' do
+      get '/vulneruby_engine/regex_dos'
+      expect(response).to(render_template(:index))
+    end
+  end
+
+  describe 'POST /regex_dos' do
+    it 'renders any loads a rendered const' do
+      post '/vulneruby_engine/regex_dos', params: { data: 'some arbitrary regex method' }
+      expect(response).to(render_template(:run))
+    end
+  end
+end
