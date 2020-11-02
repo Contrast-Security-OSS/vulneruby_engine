e80e1793e62c182862cff09afa932ef5e0d870ec
Harold McGinnis <harold.mcginnis@contrastsecurity.com>
2020-11-02 15:01:44 -0500

    RUBY-1000 :alembic: testing something

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -17,6 +17,8 @@ module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults(6.0)
+    log_path = ENV['APP_LOG'] || "log/#{Rails.env}.log"
+    config.logger = ActiveSupport::Logger.new(log_path)
 
     # Settings in config/environments/* take precedence over those specified
     # here. Application configuration can go into files in config/initializers
