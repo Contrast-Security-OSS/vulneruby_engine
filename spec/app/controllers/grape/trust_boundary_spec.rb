0ada2a01db44199534667a511204fe92f5eca905
Teodor Raychev <teodor.raychev@contrastsecurity.com>
2021-08-06 16:13:39 +0300

    specs fix

--- spec/app/controllers/grape/trust_boundary_spec.rb
+++ spec/app/controllers/grape/trust_boundary_spec.rb
@@ -3,9 +3,9 @@
 RSpec.describe('Grape trust_boundary', type: :request) do
   let(:route) { '/vulneruby_engine/grape/trust_boundary' }
 
-  describe 'GET /grape/trust_boundary' do
+  describe 'POST /grape/trust_boundary' do
     it 'renders session_id' do
-      get(route, params: { 'HTTP_USER_AGENT' => 'test user agent' })
+      post(route, params: { 'HTTP_USER_AGENT' => 'test user agent' })
       expect(response.body).to(include('session_id'))
     end
   end
