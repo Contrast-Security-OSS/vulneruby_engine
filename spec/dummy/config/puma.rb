5f75166135890856905a3b6a1b96d580749dcd43
Harold McGinnis <haroldmcginnis@Harolds-MacBook-Pro.local>
2021-09-16 09:55:47 -0400

    :construction_worker: up timeouts

--- spec/dummy/config/puma.rb
+++ spec/dummy/config/puma.rb
@@ -8,8 +8,8 @@
 #
 max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 32)
 min_threads_count = ENV.fetch('RAILS_MIN_THREADS', 8)
-worker_timeout ENV.fetch('WORKER_TIMEOUT') { 300 }
-worker_boot_timeout ENV.fetch('WORKER_TIMEOUT') { 300 }
+worker_timeout ENV.fetch('WORKER_TIMEOUT') { 600 }
+worker_boot_timeout ENV.fetch('WORKER_TIMEOUT') { 600 }
 threads(min_threads_count, max_threads_count)
 
 # Specifies the `port` that Puma will listen on to receive requests;
