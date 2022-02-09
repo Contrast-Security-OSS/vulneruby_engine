882ce7581c5bb2812aeac9cca4a755a63b75e2f5
Chris Shepardson <chris.shepardson@contrastsecurity.com>
2022-02-09 10:40:59 -0500

    add autoload controller stub, views, and route

---
+++ app/controllers/vulneruby_engine/autoload_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module VulnerubyEngine
+  # Entry point for the Insecure Hash Algorithm and Random tests
+  class AutoloadController < ApplicationController
+
+    def index
+      render('layouts/vulneruby_engine/autoload/index')
+    end
+
+    def run
+      render('layouts/vulneruby_engine/autoload/run')
+    end
+  end
+end
