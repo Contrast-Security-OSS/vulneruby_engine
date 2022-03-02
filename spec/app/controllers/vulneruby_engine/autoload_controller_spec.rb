17334d92478ff46af501f608d29b49ab7de1b1ac
Chris Shepardson <93928662+cshep-contrast@users.noreply.github.com>
2022-03-02 14:21:02 -0500

    Update spec/app/controllers/vulneruby_engine/autoload_controller_spec.rb
    Co-authored-by: Stephen Rushe <srushe@users.noreply.github.com>

--- spec/app/controllers/vulneruby_engine/autoload_controller_spec.rb
+++ spec/app/controllers/vulneruby_engine/autoload_controller_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe('Autoload Controller', type: :request) do
-  describe 'GET /included' do
+  describe 'GET /autoload' do
     it 'renders the included input page' do
       get '/vulneruby_engine/autoload'
       expect(response).to(render_template(:index))
