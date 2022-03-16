1a23adbdea202c86047c672930ff2e0041d73ec3
Kaloyan Kostadinov <kaloyan.kostadinov@contrastsecurity.com>
2022-03-16 18:13:19 +0200

    eventual sucess

--- .github/linters/install_chrome.sh
+++ .github/linters/install_chrome.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-set -ex
-apt-get -y update
-apt-get -y upgrade
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install ./google-chrome-stable_current_amd64.deb
+
+set -eo pipefail
+
+VERSION=""
+ARCH="linux64"
+
+CHROMEAPP=google-chrome
+if ! type -a google-chrome > /dev/null 2>&1; then
+    apt-get update
+    apt-get install -y google-chrome
+fi
+
+if [ "$VERSION" == "" ]; then
+    CHROME_VERSION=$("$CHROMEAPP" --version | cut -f 3 -d ' ' | cut -d '.' -f 1)
+    VERSION=$(curl --location --fail --retry 10 http://chromedriver.storage.googleapis.com/LATEST_RELEASE_${CHROME_VERSION})
+fi
+
+wget -c -nc --retry-connrefused --tries=0 https://chromedriver.storage.googleapis.com/${VERSION}/chromedriver_${ARCH}.zip
+unzip -o -q chromedriver_${ARCH}.zip
+mv chromedriver /usr/local/bin/chromedriver
+rm chromedriver_${ARCH}.zip
