b31617d26fd9f5236d612d56bcee1ca7d52cc9fb
Kaloyan Kostadinov <kaloyan.kostadinov@contrastsecurity.com>
2022-03-17 14:17:54 +0200

    change test

--- spec/app/controllers/vulneruby_engine/static_controller_spec.rb
+++ spec/app/controllers/vulneruby_engine/static_controller_spec.rb
@@ -6,30 +6,30 @@ require 'rails_helper'
 RSpec.describe('Static Controller', type: :request) do
   describe 'Static content', type: :system do
     # Set up teh Capybara & Selenium things
-    let(:driver) { Selenium::WebDriver.for :chrome }
-    let(:headless) { Headless.new }
+    # let(:driver) { Selenium::WebDriver.for :chrome }
+    # let(:headless) { Headless.new }
 
     before do
       headless.start
-    #   Capybara.register_driver :headless_chromium do |app|
-    #     options = Selenium::WebDriver::Chrome::Options.new
-    #     options.add_argument("--headless")
-    #     options.add_argument("--no-sandbox")
-    #     options.add_argument("--disable-dev-shm-usage")
-    #     options.add_argument("--window-size=1280,900")
-    #     Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: options)
-    #   end
-    #   Capybara.javascript_driver = :headless_chromium
+      Capybara.register_driver :headless_chromium do |app|
+        options = Selenium::WebDriver::Chrome::Options.new
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1280,900")
+        Capybara::Selenium::Driver.new(app, browser: :chrome, capabilities: options)
+      end
+      Capybara.javascript_driver = :headless_chromium
     end
 
-    after do
-      driver.quit
-      headless.destroy
-    end
+    # after do
+    #   driver.quit
+    #   headless.destroy
+    # end
 
     it 'verifies selenium is working' do
-      driver.visit '/vulneruby_engine/static/index'
-      expect(driver.page).to have_content('Hello world')
+      visit '/vulneruby_engine/static/index'
+      expect(page).to have_content('Hello world')
     end
   end
 end
