eda20e8df3b64fc8b84264631d757365d648e7a6
Kaloyan Kostadinov <kaloyan.kostadinov@contrastsecurity.com>
2022-03-28 19:46:28 +0300

    configure capybara for pume

--- spec/support/config/capybara.rb
+++ spec/support/config/capybara.rb
@@ -4,6 +4,11 @@ Capybara.default_driver = :rack_test
 Capybara.javascript_driver = JS_DRIVER
 Capybara.default_max_wait_time = 2
 
+Capybara.register_server :puma do |app, port, host|
+    require 'rack/handler/puma'
+    Rack::Handler::Puma.run(app, Host: host, Port: port, Threads: "0:4")
+end
+
 RSpec.configure do |config|
   config.before(:each) do |example|
     Capybara.current_driver = JS_DRIVER if example.metadata[:js]
