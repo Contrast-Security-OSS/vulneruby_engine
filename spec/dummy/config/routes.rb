724b69edcc2ec4c2704316197f92e3837a4bb733
Teodor Raychev <teodor.raychev@contrastsecurity.com>
2022-08-22 13:06:16 +0300

    Testing

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/' => 'application#home'
+  get '/' => 'application#home' if !!ENV['PASSENGER_MAX'] || !!ENV['PASSENGER_MIN']
   get '/libs/:lib' => 'library_info#info'
 
   mount VulnerubyEngine::Engine => '/vulneruby_engine'
