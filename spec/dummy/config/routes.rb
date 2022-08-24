506a42d6af6707b9b5e7326304e4295ae4f7254f
Teodor Raychev <teodor.raychev@contrastsecurity.com>
2022-08-24 13:25:22 +0300

    restoring routes

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'application#home'
+  get '/' => 'application#home'
   get '/libs/:lib' => 'library_info#info'
 
   mount VulnerubyEngine::Engine => '/vulneruby_engine'
