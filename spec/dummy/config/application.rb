ff18f4262622814413c5f6a8418a50f5a30c8b91
Kaloyan Kostadinov <kaloyan.kostadinov@contrastsecurity.com>
2022-09-12 18:06:14 +0300

    remove logger configuration of mongo

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -30,7 +30,7 @@ module Dummy
     log_path = ENV['APP_LOG'] || "log/#{ Rails.env }.log"
     config.logger = ActiveSupport::Logger.new(log_path)
 
-    config.mongoid.logger = Logger.new($stdout)
+    # config.mongoid.logger = Logger.new($stdout)
     # Settings in config/environments/* take precedence over those specified
     # here. Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
