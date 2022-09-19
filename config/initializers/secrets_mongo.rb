e6dd3c367a55241b12b3600249389f92b15feaec
Kaloyan Kostadinov <kaloyan.kostadinov@contrastsecurity.com>
2022-09-19 20:28:28 +0300

    this is it

--- config/initializers/secrets_mongo.rb
+++ config/initializers/secrets_mongo.rb
@@ -5,7 +5,6 @@ require 'vulneruby_engine/secret_mongo'
 
 return if VulnerubyEngine::SecretMongo.all.length.positive?
 
-%w[AWS GCP SUPER].each do |name|
-    $stdout.puts("gets here")
-    VulnerubyEngine::SecretMongo.create(name: "#{ name }_SECRET_API_KEY", value: SecureRandom.uuid.to_s)
-end
+# %w[AWS GCP SUPER].each do |name|
+#     VulnerubyEngine::SecretMongo.create(name: "#{ name }_SECRET_API_KEY", value: SecureRandom.uuid.to_s)
+# end
