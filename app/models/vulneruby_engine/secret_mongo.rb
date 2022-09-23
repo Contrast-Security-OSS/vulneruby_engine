7e29b1e2a85b60da3a8418c4d4364fb41e1bbb1b
Kaloyan Kostadinov <86228965+kaloyan-mm@users.noreply.github.com>
2022-09-23 16:16:16 +0300

    Update app/models/vulneruby_engine/secret_mongo.rb
    Co-authored-by: Stephen Rushe <srushe@users.noreply.github.com>

--- app/models/vulneruby_engine/secret_mongo.rb
+++ app/models/vulneruby_engine/secret_mongo.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 module VulnerubyEngine
-    class SecretMongo
-        include Mongoid::Document
-        field :name, type: String
-        field :value, type: String
-        field :created_at, :type => Date
-        field :updated_at, :type => Date
-    end
+  class SecretMongo
+    include Mongoid::Document
+    field :name, type: String
+    field :value, type: String
+    field :created_at, :type => Date
+    field :updated_at, :type => Date
+  end
 end
   
