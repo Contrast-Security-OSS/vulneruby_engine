e86b45b6f9e40f0321bdd9e35796b9e0963949f4
Teodor Raychev <teodor.raychev@contrastsecurity.com>
2022-10-28 19:42:13 +0300

    init

--- app/controllers/vulneruby_engine/nosql_injection_controller.rb
+++ app/controllers/vulneruby_engine/nosql_injection_controller.rb
@@ -9,7 +9,25 @@ module VulnerubyEngine
   
       def run
         @result = SecretMongo.where(name: params[:name]).to_a
-        render('layouts/vulneruby_engine/nosql_injection/run')
+        begin
+          puts "NOSQLI PARAM: #{ params[:name]}"
+          logger = Logger.new(STDOUT)
+          logger.level = Logger::INFO
+          client = Mongoid::Clients.default
+          client["dummy_collections", :capped => true, :size => 1024].create
+          db = client.database
+          collections = db.collection_names
+          logger.info("MongoDB #{db.name} has collections #{collections}")
+          collection = db[collections[0].to_sym]
+          doc = {"name": SecureRandom.base64(5), "date": DateTime.now, "text": SecureRandom.base64(3) }
+          collection.insert_one(doc)
+          result = collection.find(name: params[:name]).first
+          render('layouts/vulneruby_engine/nosql_injection/run')
+        rescue Mongo::Error => e
+          logger.error("[MongoError]", error: e)
+          logger.error(e.backtrace)
+          raise(e)
+        end
       end
     end
   end
