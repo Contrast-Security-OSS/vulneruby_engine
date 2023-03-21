98899017feee11ccc20df799f814c343c34e01dd
Teodor Raychev <teodor.raychev@contrastsecurity.com>
2023-03-21 18:29:21 +0200

    Refactored Debride check

--- .github/linters/debride.sh
+++ .github/linters/debride.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 OUT_FILE="debride_out.txt"
+EXPECTED_OUTPUT_FILE="debride_no_suspects.txt"
 
 bundle config set path 'vendor/bundle'
 bundle install
@@ -8,12 +9,15 @@ bundle install
 # Exclude the API classes b/c we'll never call all of them / debride has issues tracking
 bundle exec debride --whitelist ./.github/linters/debride_allowlist.txt -e ./lib/contrast/api/settings_pb.rb,./lib/contrast/api/dtm_pb.rb ./lib > $OUT_FILE
 
-# "These methods MIGHT not be called:\n" is always output
-COUNT=$(wc -l < $OUT_FILE)
-if [ $COUNT == 1 ]; then
+# inspect differences: 
+DIFF=$(diff -a --suppress-common-lines -y $OUT_FILE $EXPECTED_OUTPUT_FILE)
+LENGTH=${#DIFF}
+
+if [ $LENGTH == 0 ]; then
   echo "No dead code found"
+  echo $LENGTH
 else
-  echo "Dead code was found. See $OUT_FILE for details"
+  echo "Dead code was found. See $OUT_FILE for details:"
   cat $OUT_FILE
   exit 1
 fi
