f9cf80c86b550d2ef3c6027d1b5d7a6661c848b3
Teodor Raychev <teodor.raychev@contrastsecurity.com>
2023-03-21 16:18:57 +0200

    Added to debride allowlinst VulnerubyEngine::VERSION

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
+
 class ApplicationController < ActionController::Base
   def home
+    # Debride need this to be used:
+    VulnerubyEngine::VERSION
     render('layouts/home')
   end
 end
