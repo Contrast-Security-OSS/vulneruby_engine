87de5c32ab30e951532040c49fe0826b46c266b0
Teodor Raychev <teodor.raychev@contrastsecurity.com>
2023-03-21 18:52:21 +0200

    Updated Debride script

--- .github/linters/debride.sh
+++ .github/linters/debride.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 OUT_FILE="debride_out.txt"
-EXPECTED_OUTPUT_FILE="debride_no_suspects.txt"
 
 bundle config set path 'vendor/bundle'
 bundle install
@@ -9,15 +8,15 @@ bundle install
 # Exclude the API classes b/c we'll never call all of them / debride has issues tracking
 bundle exec debride --whitelist ./.github/linters/debride_allowlist.txt -e ./lib/contrast/api/settings_pb.rb,./lib/contrast/api/dtm_pb.rb ./lib > $OUT_FILE
 
-# inspect differences: 
-DIFF=$(diff -a --suppress-common-lines -y $OUT_FILE $EXPECTED_OUTPUT_FILE)
-LENGTH=${#DIFF}
+OUTPUT=$(cat $OUT_FILE)
 
-if [ $LENGTH == 0 ]; then
-  echo "No dead code found"
-  echo $LENGTH
-else
+# look for new output suggesting no violations are found: 
+MATCH=$(expr "$OUTPUT" : '\(.*Total suspect LOC: 0\)')
+
+if [ -z "$MATCH" ]; then
   echo "Dead code was found. See $OUT_FILE for details:"
   cat $OUT_FILE
   exit 1
+else
+  echo "No dead code found"
 fi
